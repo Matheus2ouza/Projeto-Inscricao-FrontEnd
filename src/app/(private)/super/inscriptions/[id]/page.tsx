aa08e298b243de62db84ad34188f4a7f6558c5f7
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-17 03:09:44 -0300

    refactor: remove InscriptionDetailPage component
    - Deleted the InscriptionDetailPage component as part of a cleanup process, streamlining the codebase and removing unused files.

--- src/app/(private)/super/inscriptions/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import InscriptionDetailAnalysis from "@/features/analysis/components/InscriptionDetailAnalysis";
-
-export default function InscriptionDetailPage() {
-  return <InscriptionDetailAnalysis />;
-}
