7953807fe4d3d8703ae73aef25ab38c39870301d
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-17 03:07:39 -0300

    feat: add new analysis pages for inscriptions
    - Introduced multiple new pages for detailed analysis of inscriptions, including InscriptionDetailPage, AnalysisInscriptionPage, EventInscriptionsAnalysisPage, and InscriptionDetailInsideAnalysisPage.
    - Each page imports and renders the corresponding analysis component for better organization and user navigation.

---
+++ src/app/(private)/super/inscriptions/analysis/page.tsx
@@ -0,0 +1,5 @@
+import AnalysisInscriptionTable from "@/features/analysis/components/AnalysisInscriptionTable";
+
+export default function AnalysisInscriptionPage() {
+  return <AnalysisInscriptionTable />;
+}
