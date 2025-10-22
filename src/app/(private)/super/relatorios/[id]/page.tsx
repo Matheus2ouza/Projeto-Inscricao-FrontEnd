8d5cdb3be2f9933ee87d05440b6a1127284a1e9e
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-22 17:27:09 -0300

    refactor: restructure report pages for improved organization
    - Deleted old report and report details pages under "relatorios" to streamline the codebase.
    - Introduced new report and report details pages under "report" for better clarity and separation of concerns.

--- src/app/(private)/super/relatorios/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import ReportDetails from "@/features/relatorio/components/ReportDetails";
-
-export default function RelatorioDetalhePage() {
-  return <ReportDetails />;
-}
--- src/app/(private)/super/relatorios/page.tsx
+++
@@ -1,5 +0,0 @@
-import ReportTable from "@/features/relatorio/components/ReportPage";
-
-export default function ReportPage() {
-  return <ReportTable />;
-}
